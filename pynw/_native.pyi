ddb17342173fecdff3dd9591727905b926e2da3d
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2026-03-29 00:14:23 +0100

    Fix stubs

--- pynw/_native.pyi
+++ pynw/_native.pyi
@@ -1,11 +1,6 @@
 import numpy as np
 import numpy.typing as npt
 
-def nw_traceback_indices(
-    similarity_matrix: npt.NDArray[np.float64],
-    gap_penalty_row: float,
-    gap_penalty_col: float,
-) -> tuple[float, npt.NDArray[np.intp], npt.NDArray[np.intp]]: ...
 def needleman_wunsch(
     similarity_matrix: npt.ArrayLike,
     *,
