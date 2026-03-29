efcd5dd5fcdfabe0f2f90a391b322a79f556c4a2
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2026-03-29 10:16:13 +0200

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,13 @@
 //! Safe `Array2` indexing is used throughout.  Benchmarking showed that
 //! `unsafe get_unchecked` offers no measurable improvement.
 
-use numpy::{IntoPyArray, PyArray1, PyArrayLikeDyn, PyReadonlyArray2};
+use numpy::{AllowTypeChange, IntoPyArray, PyArray1, PyArrayLikeDyn};
 use pyo3::prelude::*;
 
 mod nw_core;
 
 #[pymodule(name = "_native")]
 mod pynw_native {
-    use numpy::AllowTypeChange;
-
     use super::*;
 
     type NwTracebackIndicesOutput<'py> = (
@@ -19,19 +17,6 @@ mod pynw_native {
         Bound<'py, PyArray1<isize>>,
         Bound<'py, PyArray1<isize>>,
     );
-
-    #[pyfunction]
-    fn nw_traceback_indices<'py>(
-        py: Python<'py>,
-        similarity_matrix: PyReadonlyArray2<'py, f64>,
-        gap_penalty_row: f64,
-        gap_penalty_col: f64,
-    ) -> PyResult<NwTracebackIndicesOutput<'py>> {
-        let sm = similarity_matrix.as_array();
-        let (score, row_idx, col_idx) =
-            nw_core::nw_traceback_indices_core(&sm, gap_penalty_row, gap_penalty_col);
-        Ok((score, row_idx.into_pyarray(py), col_idx.into_pyarray(py)))
-    }
 
     /// Align two ordered sequences given a precomputed similarity matrix.
     ///
@@ -115,6 +100,7 @@ mod pynw_native {
     )]
     fn needleman_wunsch<'py>(
         py: Python<'py>,
+        // FIXME: This gives a terrible exception if conversion fails
         similarity_matrix: PyArrayLikeDyn<'py, f64, AllowTypeChange>,
         gap_penalty: f64,
         gap_penalty_row: Option<f64>,
